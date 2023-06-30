d7d40dec2dd147c852030bb7117737a9334aaa0e
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-06-30 15:32:50 +0330

    add js step 2

--- Step 2/step2.js
+++ Step 2/step2.js
@@ -1,1 +1,10 @@
+const password = ['123' ,'444','555'];
+pass = prompt('please enter password');
 
+for (let i = 0; i < password.length; i++){
+    if (pass == password [i]){
+        console.log('yes yes');
+    }else if (pass != passwora[i]){
+        console.log('no no');
+    }
+}
